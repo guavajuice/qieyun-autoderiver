94f91775fdea5dabcb9099dede0656a9ca38906c
guavajuice <thinsuu@gmail.com>
2024-09-03 16:23:56 +0800

    Update samples.ts
    改变方案

--- src/samples.ts
+++ src/samples.ts
@@ -1,26 +1,12 @@
 // prettier-ignore
 export default {
-  "切韻音系拼音或轉寫": {
-    "切韻拼音": "tupa",
-    "白一平轉寫": "baxter",
-  },
   "切韻音系擬音": {
-    "王力擬音": "wangli",
-    "unt 擬音": "unt",
-    "潘悟雲擬音": "panwuyun",
-    "msoeg 擬音": "msoeg_v8",
+    "楊力擬音": "tshertqhwirn",
   },
-  "推導後世音系": {
-    "推導盛中唐擬音": "mid_tang",
-    "推導《蒙古字韻》": "mongol",
-    "推導《中原音韻》擬音": "zhongyuan",
+
+  "切韻音系拼音或轉寫": {
+    "切韻拼音": "tshertqhwirnlaomaqzoiy",
   },
-  "現代方言推導音": {
-    "推導普通話": "putonghua",
-    "推導廣州話": "gwongzau",
-    "推導上海話": "zaonhe",
-  },
-  "人造音系": {
-    "不通話": "yec_en_hua",
-  },
+
+
 } as const;
